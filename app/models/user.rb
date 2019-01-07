fbdba179d7eb4a2ea733dfe79763f4d6a6408c03
namanh2610 <namanhhespik58@gmail.com>
2019-01-07 08:09:19 +0700

    Chapter 7: Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ApplicationRecord
-  before_save email.downcase!
+  before_save :email_downcase
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :name, presence: true, length: { maximum: Settings.user_validates.max_lenght_name }
-  validates :email, presence: true, length: { maximum: Settings.user_validates.max_lenght_email },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: Settings.users_validates.min_lenght_password }
+  validates :name, presence: true, length: { maximum: Settings.User_validates.max_lenght_name }
+  validates :email, presence: true, length: { maximum: Settings.User_validates.max_lenght_email },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :password, presence: true,
+            length: { minimum: Settings.User_validates.min_lenght_password }
 
   has_secure_password
+
+  private
+
+  def email_downcase
+    email.downcase!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  get "users/new"
   get "/help", to: "static_pages#help"
   get "/contact", to: "static_pages#contact"
   get "/about", to: "static_pages#about"
   get "/signup",  to: "users#new"
+  post "/signup",  to: "users#create"
+  resources :users
 end
