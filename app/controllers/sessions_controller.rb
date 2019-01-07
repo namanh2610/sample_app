b00534683129dcfedf9ce726d5b278f6874ff64e
namanh2610 <namanhhespik58@gmail.com>
2019-01-07 08:09:19 +0700

    Chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,29 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
 
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+      login_success user
     else
-      flash.now[:danger] = "Invalid email/password combination"
-      render :new
+      login_fail
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  def login_success user
+    log_in user
+    if (params[:session][:remember_me] == Settings.value_true)
+      remember user
+    else
+      forget user
+    end
+    redirect_to user
+  end
+
+  def login_fail
+    flash.now[:danger] = "Invalid email/password combination"
+    render :new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  attr_reader :remember_token
   before_save :email_downcase
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 
   validates :name, presence: true, length: { maximum: Settings.User_validates.max_lenght_name }
   validates :email, presence: true, length: { maximum: Settings.User_validates.max_lenght_email },
@@ -11,10 +13,30 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  def self.digest string
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
-    BCrypt::Password.create string, cost: cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update remember_digest: nil
   end
 
   private
